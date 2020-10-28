e8d699fef27b65171609635fc5cd1bf2af3acba5
Vinicius Lima <vfbdelima@gmail.com>
2020-10-28 20:57:21 -0300

    Added brightness button and crack example
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

--- app/pages/ControlPage.tsx
+++ app/pages/ControlPage.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import Logo from '../../resources/images/olha_duto_logo.svg';
+import CrackExample from '../../resources/images/crack_example.svg';
 import NavigationButton from '../components/control/ButtonNavigation';
+import BrightnessButton from '../components/control/BrightnessButton';
+import ControlPageWrapper from './styles';
 
 export default function ControlPage() {
   return (
-    <div>
-      <NavigationButton name="CÂMERA" buttonRadius={180} />
-      <NavigationButton name="PLATAFORMA" buttonRadius={180} />
-    </div>
+    <ControlPageWrapper>
+      <div style={{ flexBasis: '10%' }}>
+        <img width="180px" src={Logo} alt="logo-icon" />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexBasis: '80%',
+          justifyContent: 'space-around',
+        }}
+      >
+        <img width="62%" src={CrackExample} alt="crack-example" />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <NavigationButton name="CÂMERA" buttonRadius={140} />
+          <NavigationButton name="PLATAFORMA" buttonRadius={140} />
+        </div>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginBottom: '16px',
+        }}
+      >
+        <BrightnessButton />
+      </div>
+    </ControlPageWrapper>
   );
 }
