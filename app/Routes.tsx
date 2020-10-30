c1cbd5a44400ce2f24f63e66e16f0cb1cb3ee9fc
joaorobson <joaorobsonmartins@gmail.com>
2020-10-29 21:39:17 -0300

    Fix routes

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
+import HomePage from './containers/HomePage';
+
 
 const LazyCounterPage = React.lazy(() => import('./containers/CounterPage'));
 
--- app/pages/LoginPage.tsx
+++ app/pages/LoginPage.tsx
@@ -5,38 +5,43 @@ import '../styles/pages/LoginPage.css';
 import logoImg from '../../resources/Logo.svg';
 
 class LoginPage extends React.Component {
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.history.push('/counter');
-	};
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.history.push('/control');
+  };
 
-	render() {
-  return (
-    <main>
-      <header>
-        <img src={logoImg} alt="logo" />
-      </header>
-      <form onSubmit={this.handleSubmit}>
-        <fieldset className="input-form">
-          <input type="text" id="email-form" name="Email" placeholder="Email" />
-          <input
-            type="password"
-            id="password-form"
-            name="Senha"
-            placeholder="Senha"
-          />
-        </fieldset>
-        <fieldset className="buttons-form">
-          <input type="submit" id="button-cadastrar-form" value="CADASTRAR" />
-          <input type="submit" id="button-login-form" value="LOGIN" />
-        </fieldset>
-        <a href="random.com" id="forgotten-password">
-          Esqueci minha senha
-        </a>
-      </form>
-    </main>
-  );
-	}
+  render() {
+    return (
+      <main>
+        <header>
+          <img src={logoImg} alt="logo" />
+        </header>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset className="input-form">
+            <input
+              type="text"
+              id="email-form"
+              name="Email"
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              id="password-form"
+              name="Senha"
+              placeholder="Senha"
+            />
+          </fieldset>
+          <fieldset className="buttons-form">
+            <input type="submit" id="button-cadastrar-form" value="CADASTRAR" />
+            <input type="submit" id="button-login-form" value="LOGIN" />
+          </fieldset>
+          <a href="random.com" id="forgotten-password">
+            Esqueci minha senha
+          </a>
+        </form>
+      </main>
+    );
+  }
 }
 
 export default withRouter(LoginPage);
