fd3779e6a0ab70fd848673c451b9a14694a9758a
Vinicius Lima <vfbdelima@gmail.com>
2020-10-30 12:56:11 -0300

    Implemented page home linking with control page
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
+import HomePage from './pages/HomePage';
 
 const LazyCounterPage = React.lazy(() => import('./containers/CounterPage'));
 
