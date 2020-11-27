9a7a078512451a862ddad8747adedd7a5d6b8d17
Vinicius Lima <vfbdelima@gmail.com>
2020-11-27 00:03:01 -0300

    Added platform movement integrated with ros mock
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

--- app/components/control/ButtonNavigation.tsx
+++ app/components/control/ButtonNavigation.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ROSLIB from 'roslib';
 import PropTypes from 'prop-types';
 import { IconButton } from '@material-ui/core';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
@@ -12,52 +13,134 @@ type Props = {
   buttonRadius: number;
 };
 
-const NavigationButton = (props: Props) => {
-  const { name, buttonRadius } = props;
-  const iconButtonSize = 59;
-  const middle = buttonRadius / 2 - iconButtonSize / 2;
-  const edge = buttonRadius - iconButtonSize;
+class NavigationButton extends Component {
+  constructor(props: Props) {
+    super(props);
+    this.ros = new ROSLIB.Ros({
+      encoding: 'ascii',
+      url: 'ws://localhost:9090',
+    });
 
-  return (
-    <NavButtonWrapper>
-      <NavLabelDiv buttonRadius={buttonRadius}>{name}</NavLabelDiv>
-      <NavButtonDiv buttonRadius={buttonRadius}>
-        <IconButton
-          style={{ position: 'absolute', left: `${middle}px` }}
-          component="span"
-        >
-          <ArrowDropUpIcon fontSize="large" color="action" />
-        </IconButton>
-        <IconButton
-          style={{
-            position: 'absolute',
-            left: `${edge}px`,
-            top: `${middle}px`,
-          }}
-          component="span"
-        >
-          <ArrowRightIcon fontSize="large" color="action" />
-        </IconButton>
-        <IconButton
-          style={{
-            position: 'absolute',
-            left: `${middle}px`,
-            top: `${edge}px`,
-          }}
-          component="span"
-        >
-          <ArrowDropDownIcon fontSize="large" color="action" />
-        </IconButton>
-        <IconButton
-          style={{ position: 'absolute', top: `${middle}px` }}
-          component="span"
-        >
-          <ArrowLeftIcon fontSize="large" color="action" />
-        </IconButton>
-      </NavButtonDiv>
-    </NavButtonWrapper>
-  );
-};
+    this.state = {
+      movement: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      movementTopic: new ROSLIB.Topic({
+        ros: this.ros,
+        name: '/commands/move',
+        messageType: 'geometry_msgs/Vector3',
+      }),
+    };
+  }
+
+  componentDidMount() {
+    const { movement, movementTopic } = this.state;
+    const message = new ROSLIB.Message({
+      x: movement.x,
+      y: movement.y,
+      z: movement.z,
+    });
+    movementTopic.publish(message);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { movement, movementTopic } = this.state;
+    if (movement !== prevState.movement) {
+      const message = new ROSLIB.Message({
+        x: movement.x,
+        y: movement.y,
+        z: movement.z,
+      });
+      movementTopic.publish(message);
+    }
+  }
+
+  movePlatform = (direction) => {
+    const mapAxis = {
+      forward: 'y',
+      backwards: 'y',
+      rigth: 'x',
+      left: 'x',
+    };
+    const mapOperation = {
+      forward: 1,
+      backwards: -1,
+      rigth: 1,
+      left: -1,
+    };
+    const axis = mapAxis[direction];
+    const operation = mapOperation[direction];
+    this.setState((state) => {
+      return {
+        ...state,
+        movement: {
+          ...state.movement,
+          [axis]: (state.movement[axis] += operation),
+        },
+      };
+    });
+  };
+
+  render() {
+    const { name, buttonRadius } = this.props;
+    const iconButtonSize = 59;
+    const middle = buttonRadius / 2 - iconButtonSize / 2;
+    const edge = buttonRadius - iconButtonSize;
+    return (
+      <NavButtonWrapper>
+        <NavLabelDiv buttonRadius={buttonRadius}>{name}</NavLabelDiv>
+        <NavButtonDiv buttonRadius={buttonRadius}>
+          <IconButton
+            component="span"
+            onClick={() => {
+              this.movePlatform('forward');
+            }}
+            style={{ position: 'absolute', left: `${middle}px` }}
+          >
+            <ArrowDropUpIcon fontSize="large" color="action" />
+          </IconButton>
+          <IconButton
+            style={{
+              position: 'absolute',
+              left: `${edge}px`,
+              top: `${middle}px`,
+            }}
+            onClick={() => {
+              this.movePlatform('rigth');
+            }}
+            component="span"
+          >
+            <ArrowRightIcon fontSize="large" color="action" />
+          </IconButton>
+          <IconButton
+            style={{
+              position: 'absolute',
+              left: `${middle}px`,
+              top: `${edge}px`,
+            }}
+            onClick={() => {
+              this.movePlatform('backwards');
+            }}
+            component="span"
+          >
+            <ArrowDropDownIcon fontSize="large" color="action" />
+          </IconButton>
+          <IconButton
+            component="span"
+            onClick={() => {
+              this.movePlatform('left');
+            }}
+            style={{ position: 'absolute', top: `${middle}px` }}
+          >
+            <ArrowLeftIcon fontSize="large" color="action" />
+          </IconButton>
+        </NavButtonDiv>
+      </NavButtonWrapper>
+    );
+  }
+}
 
 NavigationButton.propTypes = {
   name: PropTypes.string,
